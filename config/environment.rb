f38fe299fb9ae553796a9f18898e01d8f5f868ff
Andres Hernandez <andres@appcoast.com>
2011-03-18 17:15:32 -0600

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+BackupS3PG::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackupS3PG::Application.config.secret_token = 'b16263dd383142f7d9da6737f5f6d2c787b34b41e1e05d4eeeeed4256dd137534c6c1c11160cc2b6e4fc79ce57ada465ec88b22967036a4d863bb13a63bf7615'
